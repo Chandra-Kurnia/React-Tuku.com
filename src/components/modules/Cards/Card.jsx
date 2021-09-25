3d0e83e7afa2daf6e886f2838e304d259485fe1c
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-09-25 22:43:10 +0700

    fix image cart strech

--- src/components/modules/Cards/Card.jsx
+++ src/components/modules/Cards/Card.jsx
@@ -5,7 +5,9 @@ import { Link } from "react-router-dom";
 export const Card = (props) => {
     return (
         <div className={`cards mt-4 me-0 me-lg-3 ${style.cardWrapper}`}>
-                <img src={`${process.env.REACT_APP_SERVER_IMAGE_URL}/${props.src}`} className="card-img-top" alt="..."></img>
+            <div className={style.imgWrapper}>
+                <img src={`${process.env.REACT_APP_SERVER_IMAGE_URL}/${props.src}`} alt="..."/>
+            </div>
                     <div className="card-body">
                         <Link to={`/product/${props.id}`}>{props.title}</Link><br />
                         <span className={style.price}>Rp. {props.price}</span><br />
